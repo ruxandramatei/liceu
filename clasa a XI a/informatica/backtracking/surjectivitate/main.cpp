d7dae2789d56924beacb989e8ef15d23ae5f9258
ruxandramatei <ruxandramatei0@gmail.com>
2016-11-19 11:55:12 +0200

    Add surjectivitate

---
+++ clasa a XI a/informatica/backtracking/surjectivitate/main.cpp
@@ -0,0 +1,44 @@
+// f : {1,2...m} --> {1,2...n}
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+ifstream in("pb.in");
+ofstream out("pb.txt");
+
+int n, m, st[10];
+
+void citire(){
+  in>>m>>n;
+}
+
+bool validare(int k){
+  for(int i = 1; i < k; i++)
+    if(st[i] == st[k])
+      return false;
+  return true;
+}
+
+void afisare(){
+  for(int i = 1; i <= n; i++)
+    out<<"f("<<st[i]<<") = "<<i<<" ; ";
+  out<<'\n';
+}
+
+void bkt(int k){
+  for(int i = 1; i<=m; i++){
+    st[k] = i;
+    if(validare(k))
+      if(k == n)
+        afisare();
+      else
+        bkt(k+1);
+  }
+}
+
+int main(){
+  citire();
+  bkt(1);
+  return 0;
+}
