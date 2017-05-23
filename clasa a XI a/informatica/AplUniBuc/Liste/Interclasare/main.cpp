5caf09ef348031f11f9f86b1b96268d099147422
ruxandramatei <ruxandramatei0@gmail.com>
2017-05-23 19:13:52 +0300

    new proj

--- clasa a XI a/informatica/AplUniBuc/Liste/Interclasare/main.cpp
+++ clasa a XI a/informatica/AplUniBuc/Liste/Interclasare/main.cpp
@@ -3,7 +3,47 @@
 
 using namespace std;
 
+ifstream in("interclasare.in");
+ofstream out("interclasare.out");
+
+//Pornind de la 2 vectori x,y ordonati crescator sa formam
+//un vector z care sa contina elementele celor 2 vectori in ordine crescatoare
+
+void citire(int v[101],int &dim){
+  in>>dim;
+  for(int i=1;i<=dim;i++)
+    in>>v[i];
+}
+
 int main(){
   int x[101], y[101], z[101];
+  int lgx, lgy, lgz = 0;
+
+  citire(x, lgx);
+  citire(y,lgy);
+
+  //interclasam vectorii
+  int i = 1, j = 1;
+  while(i<=lgx && j<=lgy){
+    if(x[i] < y[j]){//daca val x e mai mica punem in z x
+      z[++lgz] = x[i];
+      i++;
+    }
+    else{//altfel punem y in z
+      z[++lgz] = y[j];
+      j++;
+    }
+  }
+
+  while(i <= lgx)
+    z[++lgz] = x[i++];
+
+  while(j <= lgy)
+    z[++lgz] = y[j++];
+
+  out<<"Lungimea vectorului rezultat este : "<<lgz<<'\n';
+  out<<"Vectorul rezultat din interclasare este : ";
+  for(int i=1;i<=lgz;i++)
+    out<<z[i]<<' ';
   return 0;
 }
